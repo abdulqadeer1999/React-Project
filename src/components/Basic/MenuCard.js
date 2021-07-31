d5e4fc0bb43261fd3443daf60fd28f20db9ec701
abdulqadeer1999 <abdulqadeersolangi007@gmail.com>
2021-07-31 16:20:40 +0500

    menuCard

---
+++ src/components/Basic/MenuCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const MenuCard = () => {
+    return (
+        <div>
+            <div className="card-container">
+            <div className="card">
+                <div className="card-body">
+                  <span className="card-number card-circle subtle">1</span>
+                  <span className="card-author subtle">BreakFast</span>
+                  <h2 className="card-title">Maggi</h2>
+                  <span className="card-description subtle">
+                      ueu FJF FNIWFNWVBM DIWDIQDNQFNWK  Z VZVKB JUJFUFU FJUFUFU
+                  </span>
+                  <div className="card-read">READ</div>
+
+                </div>
+                
+
+            </div>
+            </div>
+        </div>
+    )
+}
--- src/components/Basic/Restaurant.js
+++ src/components/Basic/Restaurant.js
@@ -1,30 +1,16 @@
 import React, { useState } from 'react'
 import "./style.css"
 import Menu from "../menuApi"
+import MenuCard from "./MenuCard"
 
 export const Restaurant = () => {
 
-   const [menuData,setMenuData] = useState()
+   const [menuData,setMenuData] = useState(Menu)
 
 
     return (
         <div>
-            <div className="card-container">
-            <div className="card">
-                <div className="card-body">
-                  <span className="card-number card-circle subtle">1</span>
-                  <span className="card-author subtle">BreakFast</span>
-                  <h2 className="card-title">Maggi</h2>
-                  <span className="card-description subtle">
-                      ueu FJF FNIWFNWVBM DIWDIQDNQFNWK  Z VZVKB JUJFUFU FJUFUFU
-                  </span>
-                  <div className="card-read">READ</div>
-
-                </div>
-                
-
-            </div>
-            </div>
+            <MenuCard /> 
         </div>
     )
 }
